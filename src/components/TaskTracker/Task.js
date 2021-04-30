676c49a97a9cc0aa60f520a496012c7172c642d0
hanwangx <wangh109@coventry.ac.uk>
2021-04-30 12:39:57 +0100

    Connect to Firebase
    can add/delete from a real-time-database(Firestore) now.

--- src/components/TaskTracker/Task.js
+++ src/components/TaskTracker/Task.js
@@ -3,7 +3,7 @@ import {FaTimes} from 'react-icons/fa'
 
 const Task = ({task,onDelete,onToggle}) => {
     return (
-        <div className={`task ${task.reminder?'reminder':''}`} onDoubleClick={()=>(onToggle(task.id))}>
+        <div className={`task ${task.reminder?'reminder':''}`} onDoubleClick={()=>(onToggle(task))}>
             <h3 >{task.text} <FaTimes onClick={()=>(onDelete(task.id))} style={{color:'red', cursor:'pointer'}}/></h3>
             <p>{task.day}</p>
         </div>
--- src/components/TaskTracker/TaskTracker.js
+++ src/components/TaskTracker/TaskTracker.js
@@ -1,11 +1,31 @@
-import {useState} from 'react'
+import {useState,useEffect} from 'react'
 import Header from './Header'
 import Tasks from './Tasks'
 import AddTask from './AddTask'
+import firebase from '../../firebase'
+import {v4 as uuidv4} from 'uuid'
+
 
 const TaskTracker = () => {
 
     const [showAddTask, setShowAddTask]= useState(false)
+
+    const ref = firebase.firestore().collection('Tasks');
+
+    useEffect(()=>{
+        getTasks();
+    },[])
+
+    const getTasks=()=>{
+        ref.onSnapshot((querySnapshot)=>{
+            const items=[];
+            querySnapshot.forEach((doc)=>{
+                items.push(doc.data());
+            });
+            setTasks(items)
+        })
+    }
+    
 
     const [tasks,setTasks]=useState([
         {
@@ -32,17 +52,27 @@ const TaskTracker = () => {
     ])
 
     const addTask=(task)=>{
-        const id = Math.floor(Math.random()*10000)+2
+        const id = uuidv4()
         const newTask = {id,...task}
-        setTasks([...tasks, newTask])
+
+        ref.doc(id).set(newTask).catch((err)=>{console.error(err)})
+        //setTasks([...tasks, newTask])
     }
 
     const deleteTask=(id)=>{
-        setTasks(tasks.filter((task)=>(task.id!==id)))
+        ref.doc(id).delete().catch((err)=>{console.log(err)})
+        //setTasks(tasks.filter((task)=>(task.id!==id)))
     }
 
-    const reminder=(id)=>{
+    const reminder=(task)=>{
+        const id = task.id
         setTasks(tasks.map((task)=>task.id === id ? {...task, reminder: !task.reminder}: task))
+
+        ref.doc(task.id).update({
+            reminder: task.reminder
+        })
+
+        
     }
 
     return (
